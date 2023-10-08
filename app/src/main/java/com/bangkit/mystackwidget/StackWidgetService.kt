3d3368cbcb2c27bb3383276d7906f912932cc367
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-10-08 19:26:44 +0700

    first commit

---
+++ app/src/main/java/com/bangkit/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,11 @@
+package com.bangkit.mystackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+        return StackRemoteViewsFactory(this.applicationContext)
+    }
+
+}
